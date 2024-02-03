e3d28f2a881940da3a4406fd228a0b9200754252
lermal <lermalplay@mail.ru>
2024-02-03 19:25:31 +0300

    Commit

--- tools/views.py
+++ tools/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def base(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'tools/base.html', {'posts': posts})

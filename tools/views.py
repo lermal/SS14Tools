015341e1149d312cebf243a1ec202eae83a2e61a
lermal <lermalplay@mail.ru>
2024-02-03 19:08:53 +0300

    Commit

--- tools/views.py
+++ tools/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def base(request):
-    return render(request, '<h1>Hello World!</h1>', {})
+    return render(request, 'tools/base.html', {})
